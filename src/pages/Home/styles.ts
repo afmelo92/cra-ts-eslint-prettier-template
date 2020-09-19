db7f94321ef24230ec51f2668605d67d8d57b75b
Andre Melo <andre.fabian.melo@gmail.com>
2020-09-19 11:54:58 -0300

    first commit

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
-
-`;
+// eslint-disable-next-line import/prefer-default-export
+export const Container = styled.div``;
